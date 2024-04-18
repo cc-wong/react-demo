84b9c4c225950d50621445800287b4bf6690e011
Cecilia Wong <cecilia.wong.wy@gmail.com>
2024-04-18 13:09:38 +0800

    Translate schedule dates.

--- src/i18n.js
+++ src/i18n.js
@@ -4,19 +4,27 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 
 import localesConfig from './locales/config.json';
-import translationEN from './locales/en/translation.json';
-import translationZH from './locales/zh/translation.json';
-import translationJA from './locales/ja/translation.json';
+
+import moment from 'moment';
+/**
+ * A locale must be imported explicitly to include it
+ * since Create React App excludes moment locales by default.
+ * References:
+ * - https://stackoverflow.com/questions/49788259/moment-js-change-locale-not-working
+ * - https://github.com/jmblog/how-to-optimize-momentjs-with-webpack
+ */
+import 'moment/locale/zh-hk';
+import 'moment/locale/ja';
 
 const resources = {
     en: {
-        translation: translationEN
+        translation: require('./locales/en/translation.json')
     },
     zh: {
-        translation: translationZH
+        translation: require('./locales/zh/translation.json')
     },
     ja: {
-        translation: translationJA
+        translation: require('./locales/ja/translation.json')
     }
 }
 
@@ -30,10 +38,11 @@ i18n.use(initReactI18next).init({
     },
 });
 /**
- * Formats a date with the format string as defined by
- * `dateFormat.<options.format>.<lng>` in `locales/conf.json`.
+ * Formats a date according to the locale corresponding to the currently selected language
+ * with the format string as defined by `dateFormat.<options.format>.<lng>` in `locales/config.json`.
  */
 i18n.services.formatter.add('formatDate', (value, lng, options) => {
+    moment.locale(localesConfig.language[lng].localeCode);
     const format = localesConfig.dateFormat[options.format][lng];
     return dateUtils.formatDate(value, format);
 });

e64156201f3c433e3c1b70c35b216d84cdd94014
Cecilia Wong <cecilia.wong.wy@gmail.com>
2024-04-22 18:08:52 +0800

    Refresh button in error message box for API timeout. Refresh page without calling API again on language change. Code refactor on error message population.

--- src/components/SumoScheduleLookup.js
+++ src/components/SumoScheduleLookup.js
@@ -18,6 +18,7 @@ export default function SumoScheduleLookup() {
     const [year, setYear] = useState(getCurrentYear().toString());
     const [apiData, setApiData] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [refresh, setRefresh] = useState(0);
     const [error, setError] = useState(null);
 
     const { t, i18n } = useTranslation();
@@ -29,25 +30,37 @@ export default function SumoScheduleLookup() {
             if (result.success) {
                 setApiData(result.responseData);
             } else {
-                setError(getAPIErrorMessage(result, t));
+                setError(getAPIErrorMessage(result));
                 setApiData([]);
             }
         }).finally(() => setLoading(false));
-    }, [year, t]);
+    }, [year, refresh]);
 
     return (
         <div className='SumoScheduleLookup'>
             <h1 className={`title-${i18n.language}`}>{t('sumoSchedLookup.title')}</h1>
             {error && (
                 <div className='ErrorMessageBox' id='errorMessage'>
-                    <div className='ErrorMessageHeading'>{error.header}</div>
-                    {parse(error.body)}
+                    <div className='Header'>{t(error.header)}</div>
+                    {parse(t(error.body, error.bodyArgs))}
+                    {error.displayRefresh &&
+                        (<>
+                            <br />
+                            <button className='Reload' onClick={() => setRefresh(r => r + 1)}>
+                                {t('error.reload')}
+                            </button>
+                        </>)}
                 </div>
             )}
             <form name='pickYear'>
                 <YearDropdown selectedYear={year}
                     onChangeEvent={(event) => setYear(event.target.value)} />
             </form>
+            {/* <div>
+                <button onClick={() => setYear(2000)}>Test: Year 2000</button>
+                &nbsp;
+                <button onClick={() => setYear(10000)}>Test: Greater than MAX_YEAR</button>
+            </div> */}
             {loading && (
                 <div className='LoadingText' id='loadingText'>{t('loading')}</div>
             )}
@@ -60,26 +73,33 @@ export default function SumoScheduleLookup() {
  * Builds the error message to display on-screen for API call failures.
  * 
  * @param {APICallResult} result the API call result object
- * @param {*} t the translation function
- * @returns {{header: string; body: string}} the error message header and body to display
+ * @returns {{header: string; body: string; bodyArgs: *; displayRefresh: boolean}}
+ *      translation keys for the error message header and body, arguments for the message body,
+ *      and a flag to control whether to display a refresh button
  */
-const getAPIErrorMessage = (result, t) => {
-    let bodyKeys = {};
+const getAPIErrorMessage = (result) => {
+    let bodyArgs = {};
+    let displayRefresh = false;
     switch (result.error.type) {
         case APICallResult.FailType.UnsuccessfulResponse:
-            bodyKeys = {
+            bodyArgs = {
                 statusCode: result.error.statusCode,
                 statusText: result.error.statusText,
                 message: result.error.reason.message
             };
             break;
         case APICallResult.FailType.ErrorThrown:
-            bodyKeys.error = result.error.reason;
+            bodyArgs.error = result.error.reason;
+            break;
+        case APICallResult.FailType.Timeout:
+            displayRefresh = true;
             break;
         default:
     }
     return {
-        header: t(`error.message.${result.error.type}.header`),
-        body: t(`error.message.${result.error.type}.body`, bodyKeys)
+        header: `error.message.${result.error.type}.header`,
+        body: `error.message.${result.error.type}.body`,
+        bodyArgs: bodyArgs,
+        displayRefresh: displayRefresh
     };
 }
